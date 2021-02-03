b3b0c22739a25aff153a89bbf9e8a848cfd97edb
garoloup <fredwolff@free.fr>
2021-02-03 20:48:35 +0100

    mouseenter & mouseleave events

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // event ready on document
 //$(document).ready(function() {
 $(function() {
-    $("#p1").click(function() {
+    $("#p1").mouseenter(function() {
         $('#p5').hide();
+    })
+    $("#p1").mouseleave(function() {
+        $('#p5').show();
     })
 
 })
