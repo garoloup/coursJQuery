2ca6b708dd0d979937803511103ebacac888aa2a
garoloup <fredwolff@free.fr>
2021-02-04 20:23:38 +0100

    animation en //

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 $(function() {
     $("button").click(function()
                      {
-        $("div").animate({left:'600px'}, 2000, function() {
-            $("div").hide();
-        });
+        $("div").animate({
+            left:'600px',
+            opacity: 0.2,
+            width: '200px',
+            height: '300px'
+        }, 2000 );
     })
 
 })
