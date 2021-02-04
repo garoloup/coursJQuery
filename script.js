4e562599234b7b48c23a9af06777e011b8f1e3d7
garoloup <fredwolff@free.fr>
2021-02-04 20:24:43 +0100

    animate relatif

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ $(function() {
         $("div").animate({
             left:'600px',
             opacity: 0.2,
-            width: '200px',
-            height: '300px'
+            width: '+=200px',
+            height: '+=300px'
         }, 2000 );
     })
 
