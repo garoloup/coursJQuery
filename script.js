15ee48007977968b32932bf085ac67245c63147d
garoloup <fredwolff@free.fr>
2021-02-04 20:07:09 +0100

    effet slow

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 // event ready on document
 //$(document).ready(function() {
 $(function() {
-    $("#p1").on({
-        mouseenter: function() {
-            $("#p5").hide();
-        },
-        mouseleave: function() {
-            $("#p5").show();
-        },
-        click: function() {
-            $("#p4").hide();
+    $("#p1").click(function() {
+            $("#p5").hide("slow");
         }
-    })
+    )
 
 })
