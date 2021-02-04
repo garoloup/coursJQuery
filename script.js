9b63b5e177551d3f6f4e84ea06dbad35126816b9
garoloup <fredwolff@free.fr>
2021-02-04 20:14:53 +0100

    fadeToggle

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 //$(document).ready(function() {
 $(function() {
     $("#p1").click(function() {
-            $("#p5").fadeIn(1000);
+            $("#p5").fadeToggle(1000);
         }
     )
 
-    $("#p5").fadeOut();
+//    $("#p5").fadeOut();
 
 })
