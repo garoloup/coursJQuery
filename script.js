89be60b10843b50b317cc8483f954603569a9916
garoloup <fredwolff@free.fr>
2021-02-04 20:29:14 +0100

    Stop current animation

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // event ready on document
 //$(document).ready(function() {
 $(function() {
-    $("button").click(function()
+    $("#start").click(function()
                      {
         $("div").animate({left:'600px',}, 2000 );
         $("div").animate({opacity:'0.2',}, 2000 );
         $("div").animate({width:'200px',}, 2000 );
         $("div").animate({height:'200px',}, 2000 );
-    })
+    });
+
+    $("#stop").click(function() {
+        $("div").stop();
+
+    });
+
 
 })
