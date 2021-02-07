1b10df7e610552f2bbd3a1253b16adf507eb0e6e
garoloup <fredwolff@free.fr>
2021-02-07 18:38:28 +0100

    get content of tags or input

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // event ready on document
 //$(document).ready(function() {
 $(function() {
-    var mylog = $("#p1").text();
-    console.log(mylog);
 
+    $("#start").click( function() {
+        var mylog = $("#p1").text();
+        console.log(mylog);
+        mylog = $("#p1").html();
+        console.log(mylog);
+        mylog = $("div").html();
+        console.log(mylog);
+
+        var myContent = $("#myInput").val();
+        console.log(myContent);
+
+    })
 
 })
