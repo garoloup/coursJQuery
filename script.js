dec4a5d7658e63dcaffd47549bd75bd5bfa8dbbf
garoloup <fredwolff@free.fr>
2021-02-08 20:11:19 +0100

    Add css attr in tag

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $(function() {
 
     $("#act1").click( function() {
-        $("#p2,#p4").toggleClass("red ");
+        $("#p2,#p4").css("color","red");
 
     })
     $("#act2").click( function() {
